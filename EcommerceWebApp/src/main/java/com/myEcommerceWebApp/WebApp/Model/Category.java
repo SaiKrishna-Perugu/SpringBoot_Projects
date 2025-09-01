691e95ff8ff81af55614cebd29f04276eac9fd73
Sai Krishna Perugu <p.saikrishna355@gmail.com>
2025-09-01 20:57:47 +0530

    EcommerceWebAppApplication - Developed model classes: Category, Product, Image with JPA annotations

---
+++ EcommerceWebApp/src/main/java/com/myEcommerceWebApp/WebApp/Model/Category.java
@@ -0,0 +1,30 @@
+package com.myEcommerceWebApp.WebApp.Model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products;
+}
---
+++ EcommerceWebApp/src/main/java/com/myEcommerceWebApp/WebApp/Model/Image.java
@@ -0,0 +1,37 @@
+package com.myEcommerceWebApp.WebApp.Model;
+
+import java.sql.Blob;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob imageData;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    // Constructors, getters, and setters
+}
